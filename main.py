a8b54935d676cb3e98dc82883c73b58531ee67a9
[normatovbekzod] <[bekzod@u.yale-nus.edu.sg]>
2023-11-14 21:04:37 +0800

    Fix port in Flask app

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import os
 
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
  # Create an object of the class Flask
 
 app = Flask(__name__)
@@ -58,7 +60,5 @@ def predict():
     hiv_test_pred = model.predict(df)
     return render_template('index.html', prediction_text = f'Your chance of being at risk:{hiv_test_pred}')
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__=='__main__':
+    app.run(debug=True)
